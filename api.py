ac401849d4a30f20a0f2a0437198dbbf6b664548
Tushar <goyalt796@gmail.com>
2024-11-09 00:30:32 +0530

    gitignore

--- api.py
+++ api.py
@@ -1,15 +1,22 @@
 from openai import OpenAI
 import os
+from dotenv import load_dotenv
 
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key="sk-proj-WJJ2Eqlv9mt1wECfSZQrT3BlbkFJg7mJikxewEyY7S9iOjWb"
-)
+load_dotenv()
+
+api = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api)
+
+email_type = input("What type of email do you want to write? ")
 response = client.chat.completions.create(
     messages=[
         {
+            "role": "system",
+            "content": "you are a expert in writing an email, whatever kind of email it is, you can write it in a professional way and make it look good and original giving it a little human touch.",
+        },
+        {
             "role": "user",
-            "content": "write an email to my boss asking for a raise",
+            "content": f"I need to write an email of {email_type}",
         }
     ],
     model="gpt-3.5-turbo",
